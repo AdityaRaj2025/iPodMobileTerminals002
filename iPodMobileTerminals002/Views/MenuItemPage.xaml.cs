6eecd79fd135484d51d69fd7738edfe1a11d15cf
Aditya <adityaraj19997@gmail.com>
2021-12-22 21:54:08 +0530

    Updated

---
+++ iPodMobileTerminals002/Views/MenuItemPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using iPodMobileTerminals002.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace iPodMobileTerminals002.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuItemPage : ContentPage
+    {
+        
+        public MenuItemPage()
+        {
+            InitializeComponent();
+            Navigation.PopAsync(true);
+            BindingContext = new MenuItemViewModel();            
+        }
+
+        private void Button_Clicked_Category(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CategoryPage(), true);
+        }
+
+        void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var previous = e.PreviousSelection;
+            var current = e.CurrentSelection;                      
+        }
+
+    }
+}
